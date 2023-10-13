a83151ee700167c6e0181058b79c883f4f8d3ba4
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-13 18:58:54 +0300

    client/routes/layout/navbar

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Layout = () => {
-  return <div>Layout</div>;
+import Navbar from "./Navbar";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <div className="container mx-auto">
+        <Navbar />
+        {children}
+      </div>
+    </>
+  );
 };
 
 export default Layout;
